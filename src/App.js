11ad1cce6979cd04efabc9832a9bd3cd9cb9874c
Marlon Almino <alminomarlon@gmail.com>
2022-11-17 22:07:55 -0300

    Números Aleatórios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import {View, StyleSheet} from 'react-native'
 
-import MinMax from './components/MinMax'
+import Aleatorio from './components/Aleatorio'
+// import MinMax from './components/MinMax'
 // import CompPadrao, {Comp1, Comp2} from './components/Multi'
 // import Primeiro from './components/Primeiro'
 
 export default () => (
   <View style={style.App}>
-    <MinMax min={3} max={20} />
+    <Aleatorio min={10} max={60} />
+    <Aleatorio min={10} max={60} />
+    <Aleatorio min={10} max={60} />
+    <Aleatorio min={10} max={60} />
+    <Aleatorio min={10} max={60} />
+    <Aleatorio min={10} max={60} />
+    {/* <MinMax min={3} max={20} />
     <MinMax min={1} max={94} />
-    {/* <CompPadrao />
+    <CompPadrao />
     <Comp1 />
     <Comp2 />
     <Primeiro /> */}
