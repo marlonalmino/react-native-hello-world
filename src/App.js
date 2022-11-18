952824a498f5514eb7a6a5967d3fd7d6b2c97a2d
Marlon Almino <alminomarlon@gmail.com>
2022-11-18 18:42:43 -0300

    React Fragment

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import {View, StyleSheet} from 'react-native'
 
-import Aleatorio from './components/Aleatorio'
+import Titulo from './components/Titulo'
+// import Aleatorio from './components/Aleatorio'
 // import MinMax from './components/MinMax'
 // import CompPadrao, {Comp1, Comp2} from './components/Multi'
 // import Primeiro from './components/Primeiro'
 
 export default () => (
   <View style={style.App}>
+    <Titulo
+      principal="Cadastro de Produto"
+      secundario="Tela de Cadastro do produto"
+    />
+    {/*<Aleatorio min={10} max={60} />
     <Aleatorio min={10} max={60} />
     <Aleatorio min={10} max={60} />
     <Aleatorio min={10} max={60} />
     <Aleatorio min={10} max={60} />
     <Aleatorio min={10} max={60} />
-    <Aleatorio min={10} max={60} />
-    {/* <MinMax min={3} max={20} />
+    <MinMax min={3} max={20} />
     <MinMax min={1} max={94} />
     <CompPadrao />
     <Comp1 />
