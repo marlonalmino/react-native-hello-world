fe2c600af0a5565f5487255dc5273f234bba57aa
Marlon Almino <alminomarlon@gmail.com>
2022-11-25 10:53:52 -0300

    Usando Comunicação Direta e Indireta

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import {View, StyleSheet} from 'react-native'
 
-import Pai from './components/indireta/Pai'
+import ContadorV2 from './components/contador/ContadorV2'
+// import Pai from './components/indireta/Pai'
 // import Contador from './components/Contador'
 // import Botao from './components/Botao'
 // import Titulo from './components/Titulo'
@@ -12,8 +13,9 @@ import Pai from './components/indireta/Pai'
 
 export default () => (
   <View style={style.App}>
-    <Pai />
+    <ContadorV2 />
     {/*
+    <Pai />
     <Contador inicial={100} passo={13} />
     <Contador />
     <Botao />
