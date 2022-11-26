bc7322149fad68c9491abd3761c1b365640cdf46
Marlon Almino <alminomarlon@gmail.com>
2022-11-26 18:58:00 -0300

    Componente Mega-Sena #02

--- src/components/mega/Mega.js
+++ src/components/mega/Mega.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import { Button, Text, TextInput } from 'react-native'
+import { Button, Text, TextInput, View } from 'react-native'
 import Estilo from '../estilo'
+
+import MegaNumero from './MegaNumero'
 
 export default class Mega extends Component {
 
@@ -27,6 +29,13 @@ export default class Mega extends Component {
     this.setState({ numeros })
   }
 
+  exibirNumeros = () => {
+    const nums = this.state.numeros
+    return nums.map(num => {
+      return <MegaNumero key={num} num={num} />
+    })
+  }
+
   render() {
     return (
       <>
@@ -35,7 +44,7 @@ export default class Mega extends Component {
         </Text>
         <TextInput
           keyboardType={'numeric'}
-          style={{ borderBottomWidth: 1 }}
+          style={{ borderBottomWidth: 1, marginBottom: 20 }}
           placeholder='Qtde de Números'
           value={this.state.qtdeNumeros}
           onChangeText={this.alterarQtdeNumero}
@@ -44,9 +53,15 @@ export default class Mega extends Component {
           title='Gerar'
           onPress={this.gerarNumeros}
         />
-        <Text>
-          {this.state.numeros.join(', ')}
-        </Text>
+        <View style={{
+          marginTop: 20,
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'center'
+        }}>
+
+          {this.exibirNumeros()}
+        </View>
       </>
     )
   }
